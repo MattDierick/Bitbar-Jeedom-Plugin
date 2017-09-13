9f3afbae2f2c0029c821080e379266decfaa146c
Matthieu Dierick <dierick@PAR-ML-DIERICK.local>
2017-09-13 11:35:38 +0200

    New image

--- jeedom.py
+++ jeedom.py
@@ -6,7 +6,7 @@
 # <bitbar.author.github>MattDierick</bitbar.author.github>
 # <bitbar.desc>Keep an eye on Jeedom devices</bitbar.desc>
 # <bitbar.dependencies>python</bitbar.dependencies>
-# <bitbar.image>https://www.jeedom.com/site/logo.png</bitbar.image>
+# <bitbar.image>https://github.com/MattDierick/Bitbar-Jeedom-Plugin/blob/master/images/screenshot.png</bitbar.image>
 
 import urllib2
 
